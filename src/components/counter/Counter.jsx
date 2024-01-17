c8915dca9eaa3e9e6c36b30bbfd137f7e5c80cf4
fsampson30 <fsampson30@gmail.com>
2024-01-17 07:51:59 -0300

    Added reset button

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -14,12 +14,17 @@ export default function Counter() {
         setCount(count - by)
     }
 
+    function resetCounter(){
+        setCount(0)
+    }
+
     return (
         <>
             <span className="totalCount">{count}</span>
             <CounterButton by={1} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
             <CounterButton by={2} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
             <CounterButton by={5} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
+            <button className="resetButton" onClick={resetCounter}>Reset</button>
         </>
     )
 }
