ceeaf940322c9b10c1eb59a16ff996865bb74650
fsampson30 <fsampson30@gmail.com>
2024-01-26 07:48:06 -0300

    Created logic to navigate when authenticated.

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './TodoApp.css'
 import LogoutComponent from '../todo/LogoutComponent'
 import HeaderComponent from './HeaderComponent'
@@ -7,7 +7,14 @@ import ListTodosComponent from './ListTodosComponent'
 import ErrorComponent from './ErrorComponent'
 import WelcomeComponent from './WelcomeComponent'
 import LoginComponent from './LoginComponent'
-import AuthProvider from './security/AuthContext'
+import AuthProvider, { useAuth } from './security/AuthContext'
+
+function AuthenticatedRoute({ children }) {
+    const authContext = useAuth()
+    if (authContext.isAuthenticated)
+        return children
+    return <Navigate to="/" />
+}
 
 export default function TodoApp() {
     return (
@@ -18,9 +25,18 @@ export default function TodoApp() {
                     <Routes>
                         <Route path='/' element={<LoginComponent />} />
                         <Route path='/login' element={<LoginComponent />} />
-                        <Route path='/welcome/:username' element={<WelcomeComponent />} />
-                        <Route path='/todos' element={<ListTodosComponent />} />
-                        <Route path='logout' element={<LogoutComponent />} />
+                        <Route path='/welcome/:username' element={
+                            <AuthenticatedRoute>
+                                <WelcomeComponent />
+                            </AuthenticatedRoute>} />
+                        <Route path='/todos' element={
+                            <AuthenticatedRoute>
+                                <ListTodosComponent />
+                            </AuthenticatedRoute>} />
+                        <Route path='logout' element={
+                            <AuthenticatedRoute>
+                                <LogoutComponent />
+                            </AuthenticatedRoute>} />
                         <Route path='*' element={<ErrorComponent />} />
                     </Routes>
                 </BrowserRouter>
