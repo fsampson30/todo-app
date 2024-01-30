c62af4d068e9852bac26f079a5194f6689624f2d
fsampson30 <fsampson30@gmail.com>
2024-01-30 07:29:00 -0300

    Create logic to return data from 2 types of API

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,20 +1,28 @@
 import { useParams, Link } from "react-router-dom"
 import axios from "axios"
+import { useState } from "react"
 
 function WelcomeComponent() {
 
     const { username } = useParams()
 
+    const [message, setMessage] = useState(null)
+
     function callHelloWorldRestApi(){        
-        axios.get('http://localhost:8080/hello-world',{},{basicAuth:{username: 'user',password:'testing@123'}})
+        //axios.get('http://localhost:8080/hello-world')
+        //.then((response) => successfulResponse(response))
+        //.catch((error) => errorResponse(error))
+        //.finally(() => console.log('cleanup'))
+     
+        axios.get('http://localhost:8080/hello-world-bean')
         .then((response) => successfulResponse(response))
         .catch((error) => errorResponse(error))
         .finally(() => console.log('cleanup'))
-
     }
 
     function successfulResponse(response){
         console.log(response)
+        setMessage(response.data.message)
     }
 
     function errorResponse(error){
@@ -30,6 +38,7 @@ function WelcomeComponent() {
             <div>
                 <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>Call Hello World</button>
             </div>
+            <div className="text-info">{message}</div>
         </div>
     )
 }
