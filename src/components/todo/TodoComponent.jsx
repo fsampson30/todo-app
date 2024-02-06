de5b85d7fa7718498cde24fd658ba38ce111781d
fsampson30 <fsampson30@gmail.com>
2024-02-06 07:32:10 -0300

    Added form validation

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom"
 import { retrieveTodo } from "./api/TodoApiService"
 import { useAuth } from "./security/AuthContext"
 import { useEffect, useState } from "react"
-import { Field, Form, Formik } from "formik"
+import { ErrorMessage, Field, Form, Formik } from "formik"
 
 export default function TodoComponent() {
 
@@ -27,14 +27,38 @@ export default function TodoComponent() {
 
     }
 
+    function validate(values){
+        let errors = {
+            //description: 'Enter a valid description',
+            //targetDate: 'Enter a valid date'
+        }
+        if(values.description.length<5){
+            errors.description = 'Enter at least 5 characters'
+        }
+        if(values.description.targetDate == null){
+            errors.targetDate = 'Enter a valid date'
+        }
+        return errors
+    }
+
     return (
         <div className="container">
             <h1>Enter Todo Details</h1>
             <div>
-                <Formik initialValues={{description,targetDate}} enableReinitialize={true} onSubmit={onSubmit}>
+                <Formik initialValues={{description,targetDate}} enableReinitialize={true} onSubmit={onSubmit} validate={validate} validateOnChange={false} validateOnBlur={false}>
                     {
                         (props) => (
                             <Form>
+                                <ErrorMessage
+                                    name="description"
+                                    component="div"
+                                    className="alert alert-warning"
+                                 />
+                                <ErrorMessage
+                                    name="targetDate"
+                                    component="div"
+                                    className="alert alert-warning"
+                                 />
                                 <fieldset className="form-group">
                                     <label>Description</label>
                                     <Field type="text" className="form-control" name="description" />
